d36627f5656665ec3d16b82e7836f444d4d11c95
Ester Hugosson <eh225bx@student.lnu.se>
2025-05-17 14:37:15 +0200

    Remove app instance here and import it instead

--- src/server.js
+++ src/server.js
@@ -1,56 +1,12 @@
-/**
- * @file Defines the main application.
- * @module src/server
- * @author Ester Hugosson
- * @version 1.0.0
- */
-
-import httpContext from 'express-http-context'
-import express from 'express' 
-import '@lnu/json-js-cycle'
-import cors from 'cors'
-import helmet from 'helmet'
-import { connectToDatabase } from './config/mongoose.js'
-import { requestContext } from './middlewares/requestContext.js'
-import { morganLogger } from './config/morgan.js'
+// src/server.js
+import { app } from './app.js'
 import { logger } from './config/winston.js'
-import { router } from './routes/router.js'
-import { errorHandler } from './middlewares/errorHandler.js'
-
+import { connectToDatabase } from './config/mongoose.js'
 
 try {
-  // Connect to MongoDB.
   await connectToDatabase(process.env.DB_CONNECTION_STRING)
 
-  // Create an Express application.
-  const app = express()
-
-  // Set various HTTP headers to make the application little more secure (https://www.npmjs.com/package/helmet).
-  app.use(helmet())
-
-  // Enable Cross Origin Resource Sharing (CORS) (https://www.npmjs.com/package/cors).
-  app.use(cors())
-
-  // Parse requests of the content type application/json.
-  app.use(express.json())
-
-  // Add the request-scoped context.
-  app.use(httpContext.middleware)
-
-// Middleware to be executed before the routes.
-  app.use(requestContext) 
-
-  // Use a morgan logger.
-  app.use(morganLogger)
-
-  // Register routes.
-  app.use('/', router)
-
-  // Error handler.
-  app.use(errorHandler)
-
-  // Starts the HTTP server listening for connections.
-  const server = app.listen(process.env.PORT, () => {
+  const server = app.listen(process.env.PORT || 3000, () => {
     logger.info(`Server running at http://localhost:${server.address().port}`)
     logger.info('Press Ctrl-C to terminate...')
   })
