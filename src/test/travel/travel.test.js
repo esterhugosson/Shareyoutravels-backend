e387686e05088766dbe3c8dc3ca9310508f81b2d
Ester Hugosson <eh225bx@student.lnu.se>
2025-05-20 21:10:47 +0200

    Add tests for read and create travel

--- src/test/travel/travel.test.js
+++ src/test/travel/travel.test.js
@@ -11,6 +11,7 @@ dotenv.config()
 let mongoServer
 let token
 let userId
+// let travelId
 
 describe('Travel CRUD application', () => {
   // ---Connect to database---//
@@ -46,6 +47,7 @@ describe('Travel CRUD application', () => {
     await mongoServer.stop()
   }, 15000) // timeout 15 sec
 
+  // ---CREATE--- //
   it('should create a travel for authenticated user', async () => {
     const res = await request(app)
       .post('/api/v1/travels') // adjust path if needed
@@ -65,7 +67,72 @@ describe('Travel CRUD application', () => {
 
       })
 
+    const travelId = res.body.id
+    console.log('Travel id is' + travelId)
+
     expect(res.statusCode).toBe(201)
     expect(res.body.travel.destination).toBe('Rome')
+  }, 15000)
+
+  // ---CREATE--- //
+  it('should NOT create a travel for unauthenticated user', async () => {
+    const res = await request(app)
+      .post('/api/v1/travels')
+      .send({
+        destination: 'Rome',
+        transport: 'flight', // Make sure this matches the enum: lowercase
+        notes: 'Test travel',
+        places: [],
+        isPublic: true,
+        startDate: '2025-06-01',
+        endDate: '2025-06-15',
+        location: {
+          lat: 41.9028, // Rome's latitude
+          lng: 12.4964 // Rome's longitude
+        }
+
+      })
+
+    expect(res.statusCode).toBe(401)
+  }, 15000)
+
+  // ---CREATE--- //
+  it('should NOT create a travel with missing parameters', async () => {
+    const res = await request(app)
+      .post('/api/v1/travels')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        destination: 'Rome',
+        notes: 'Test travel',
+        places: [],
+        isPublic: true,
+        startDate: '2025-06-01',
+        endDate: '2025-06-15',
+        location: {
+          lat: 41.9028, // Rome's latitude
+          lng: 12.4964 // Rome's longitude
+        }
+
+      })
+
+    expect(res.statusCode).toBe(400)
+  }, 15000)
+
+  // ---READ--- //
+  it('Should get my own travels', async () => {
+    const res = await request(app)
+      .get('/api/v1/travels')
+      .set('Authorization', `Bearer ${token}`)
+
+    expect(res.statusCode).toBe(200)
+  }, 15000)
+
+  // ---READ--- //
+  it('Should get all public travels', async () => {
+    const res = await request(app)
+      .get('/api/v1/travels/allTravels')
+      .set('Authorization', `Bearer ${token}`)
+
+    expect(res.statusCode).toBe(200)
   }, 15000)
 })
